c406320e4e6eecf25a453036b7cc2dd6fb14d519
Rathy23 <thyjks2020@gmail.com>
2025-06-08 21:40:25 +0700

    Fix Error API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-    API_BASE_URL:"http://127.0.0.1:8000/api",
-    IMAGE_BASE_URL:"http://127.0.0.1:8000/storage",
+    // API_BASE_URL:"http://127.0.0.1:8000/api",
+    // IMAGE_BASE_URL:"http://127.0.0.1:8000/storage",
+    API_BASE_URL:"https://laormerl-admin.checkplify168.xyz/api",
+    IMAGE_BASE_URL:"https://laormerl-admin.checkplify168.xyz/storage"
 }
