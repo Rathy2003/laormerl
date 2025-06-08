2945c3ca2b33d24f1afca02756ed1b19f5d0b0f7
Rathy23 <thyjks2020@gmail.com>
2025-06-08 21:22:17 +0700

    Fix Errir

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    API_BASE_URL:"https://laormerlbackend.duckdns.org/api",
+    IMAGE_BASE_URL:"https://laormerlbackend.duckdns.org/storage"
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    // API_BASE_URL:"http://127.0.0.1:8000/api",
-    API_BASE_URL:"https://laormerlbackend.duckdns.org/api",
-    // IMAGE_BASE_URL:"http://127.0.0.1:8000/storage",
-    IMAGE_BASE_URL:"https://laormerlbackend.duckdns.org/storage"
+    API_BASE_URL:"http://127.0.0.1:8000/api",
+    IMAGE_BASE_URL:"http://127.0.0.1:8000/storage",
 }
